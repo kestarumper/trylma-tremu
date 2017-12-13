2c7f64782f9ad268c76383e504ddba09cca7f453
Adrian Mucha <kestarumper8@gmail.com>
2017-12-13 01:42:12 +0100

    canvas painted clock

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -20,8 +20,4 @@ public class HomeController extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
-    public Result canvas() {
-        return ok(canvas.render("Your new application is ready."));
-    }
-
 }
