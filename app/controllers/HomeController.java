2127f94af91e4a23fa433316533165018d694a28
Adrian Mucha <kestarumper8@gmail.com>
2017-12-13 01:22:16 +0100

    added /canvas view

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -20,4 +20,8 @@ public class HomeController extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public Result canvas() {
+        return ok(canvas.render("Your new application is ready."));
+    }
+
 }
