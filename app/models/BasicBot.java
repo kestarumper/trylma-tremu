e11d0bc6ae2918689335fe6bf812e3a5630949c8
Adrian Mucha <kestarumper8@gmail.com>
2018-01-03 16:53:23 +0100

    bot init, added todos xD

--- app/models/BasicBot.java
+++ app/models/BasicBot.java
@@ -1,15 +1,54 @@
 package models;
 
-import akka.actor.ActorRef;
+import models.Field.Field;
+import models.Pawn.Pawn;
+
+import java.util.ArrayList;
 
 public class BasicBot extends User {
+    private final GameBoard gameBoard;
+    private boolean initialized;
+    private ArrayList<Pawn> botPawns;
+    private ArrayList<Field> fieldsToAcheive;
 
-    public BasicBot(String name, String csrf) {
+    public BasicBot(String name, String csrf, GameBoard gameBoard) {
         super(name.concat("Bot"), csrf);
+        this.gameBoard = gameBoard;
+        this.initialized = false;
+
+
+
+    }
+
+    private void initializeBot(){
+        this.botPawns = calculateSelfPawns();
+        this.fieldsToAcheive = calculateFieldsToAcheive();
+
+    }
+
+    private ArrayList<Field> calculateAvailableMoves() {
+        // TODO: wszystkie mozliwe ruchy dookoła
+        return null;
+    }
+
+    private double calculateDistance(Field candidate, Field destination) {
+        // TODO: dystans
+        return 0;
+    }
+
+    private Field getDestinationField(ArrayList<Field> winingFields){
+        //TODO: oblicz dystans dla kazdego i zwroc najlepszego
+
+        return null;
     }
 
     public String action(GameBoard gameBoard) {
         // TODO: make bot decide and return what to do
+        if(!initialized){
+            initializeBot();
+            this.initialized = true;
+        }
+
 
 //        var moves = {
 //                'type' : "move",
@@ -21,5 +60,45 @@ public class BasicBot extends User {
 //};
 
         return "{ \"type\" : \"pass\" }";
+    }
+
+    private ArrayList<Pawn> calculateSelfPawns(){
+        ArrayList<Pawn> tempArray = new ArrayList<>();
+
+        String ourColor = this.color;
+        int x = this.gameBoard.sizeOfX;
+        int y = this.gameBoard.sizeOfY;
+
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                Pawn tempPawn = this.gameBoard.getField(i, j).getPawn();
+                if(tempPawn != null){
+                    if(tempPawn.getColor().equals(ourColor)){
+                        tempArray.add(tempPawn);
+                    }
+                }
+            }
+        }
+
+        return tempArray;
+    }
+
+    public ArrayList<Field> calculateFieldsToAcheive() {
+        ArrayList<Field> tempArray = new ArrayList<>();
+
+        String desiredColor = this.botPawns.get(0).getDesiredColor();
+        int x = this.gameBoard.sizeOfX;
+        int y = this.gameBoard.sizeOfY;
+
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                String fieldColor = gameBoard.getFieldType(i, j);
+                if(fieldColor.equals(desiredColor)){
+                    tempArray.add(gameBoard.getField(i, j));
+                }
+            }
+        }
+
+        return tempArray;
     }
 }
--- test/ModelTests/GameBoardTest.java
+++ test/ModelTests/GameBoardTest.java
@@ -6,7 +6,6 @@ import models.Field.UnavailableField;
 import models.GameBoard;
 import models.GameBoardGenerators.SixPlayerBoard;
 import models.PawnMove.BasicMove;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -28,20 +27,20 @@ public class GameBoardTest {
     @Test(expected = IllegalArgumentException.class)
     public void negativeCordShouldRaiseError(){
         GameBoard board = new GameBoard(3, new BasicMove(), new SixPlayerBoard());
-        String test = board.getField(-15, 20);
+        String test = board.getFieldType(-15, 20);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void toBigNumberOutOfBoundsOfArrayRaiseException(){
         GameBoard board = new GameBoard(3, new BasicMove(), new SixPlayerBoard());
-        String test = board.getField(5433, 543);
+        String test = board.getFieldType(5433, 543);
     }
 
     @Test
     public void zeroCordSHouldBeUnavilable(){
         GameBoard board = new GameBoard(2, new BasicMove(), new SixPlayerBoard());
         Field test = new UnavailableField(new BasicField());
-        assertEquals(board.getField(0, 0), test.getType());
+        assertEquals(board.getFieldType(0, 0), test.getType());
     }
 
 }
